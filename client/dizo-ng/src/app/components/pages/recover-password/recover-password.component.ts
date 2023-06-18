a20946f6af77fd3095a67032ed876cdff3ba58cb
tomcruzana <tomcruzana@ymail.com>
2023-06-17 17:04:21 -0700

    SMOXY-36 Refactored Recover Password Form

--- client/dizo-ng/src/app/components/pages/recover-password/recover-password.component.ts
+++ client/dizo-ng/src/app/components/pages/recover-password/recover-password.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-recover-password',
-  templateUrl: './recover-password.component.html',
-  styleUrls: ['./recover-password.component.scss']
+    selector: "app-recover-password",
+    templateUrl: "./recover-password.component.html",
+    styleUrls: ["./recover-password.component.scss"],
 })
 export class RecoverPasswordComponent implements OnInit {
+    form = {
+        email: "",
+    };
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    onSubmit(): void {
+        // TODO : implement this on using a POST service
+        console.log(JSON.stringify(this.form, null, 2));
+    }
 }
