0eb9e79c3941f296f468198090e8b002d18bf1b7
tomcruzana <tomcruzana@ymail.com>
2023-06-24 23:05:37 -0700

    updated profile pages

--- client/dizo-ng/src/app/components/pages/user-profile/user-profile.component.ts
+++ client/dizo-ng/src/app/components/pages/user-profile/user-profile.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+    selector: "app-user-profile",
+    templateUrl: "./user-profile.component.html",
+    styleUrls: ["./user-profile.component.scss"],
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+    // TODO: check https://www.youtube.com/watch?v=5yf29GZ9Dac
+    states = [
+        { id: "dc", name: "dc" },
+        { id: "md", name: "md" },
+        { id: "va", name: "va" },
+    ];
 
+    form = {
+        fname: "",
+        lname: "",
+        email: "",
+        company: "",
+        countryCode: "",
+        phoneNumber: "",
+        receiveEmail: false,
+        receiveText: false,
+        shippingAddress: {
+            addressLine1: "",
+            addressLine2: "",
+            city: "",
+            stateInput: "",
+            zipCode: "",
+            country: "",
+        },
+    };
+
+    ngOnInit(): void {
+        this.form.countryCode = "+1";
+        this.form.shippingAddress.country = "USA";
+    }
+
+    onSubmit(): void {
+        // TODO : implement this on using a POST service
+        console.log(JSON.stringify(this.form, null, 2));
+    }
+
+    // resetForm(form: NgForm): void {
+    //     form.reset();
+    // }
 }
