6191bcd9f0f39912ac10c610574772c315fc3ed3
tomcruzana <tomcruzana@ymail.com>
2023-06-28 09:50:56 -0700

    converted contact us form into a template-driven form

--- client/dizo-ng/src/app/components/pages/contact/contact.component.ts
+++ client/dizo-ng/src/app/components/pages/contact/contact.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+    selector: "app-contact",
+    templateUrl: "./contact.component.html",
+    styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
+    form = {
+        fname: "",
+        lname: "",
+        email: "",
+        phoneNumber: "",
+        subject: "",
+        category: "",
+        message: "",
+        agreeTerms: false,
+    };
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    onSubmit(): void {
+        // set the eula - terms agreement to true upon submit
+        this.form.agreeTerms = true;
+
+        // TODO : implement this on using a POST service
+        console.log(JSON.stringify(this.form, null, 2));
+    }
+
+    // resetForm(form: NgForm): void {
+    //     form.reset();
+    // }
 }
