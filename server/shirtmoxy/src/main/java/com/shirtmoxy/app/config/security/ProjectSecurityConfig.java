e229b248a66ca8597d3887652b3bc28396565c84
tomcruzana <tomcruzana@ymail.com>
2023-07-15 23:59:09 -0700

    SMOXY-52 integrated security classes in backend

---
+++ server/shirtmoxy/src/main/java/com/shirtmoxy/app/config/security/ProjectSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.shirtmoxy.app.config.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setExposedHeaders(Arrays.asList("Authorization"));
+                config.setMaxAge(3600L);
+                return config;
+            }
+        })).csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler)
+        		.ignoringRequestMatchers("/contact","/sign-up", "/subscribe")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests((requests)->requests
+                        .requestMatchers("/user/projects").hasRole("USER")
+                        .requestMatchers("/user/profile").hasRole("USER")
+                        .requestMatchers("/user/password-reset").hasRole("USER")
+                        .requestMatchers("/user/manage-account").hasRole("USER")
+                        .requestMatchers("/user").authenticated()
+                        .requestMatchers("/home", "/product/*", "/about", "/contact", "/faq", "/shipping-and-returns", "/sign-in", "/sign-up", "/sign-out", "/recover-password", "/track", "/cart", "/checkout", "/subscribe", "/terms-condition", "/privacy-policy", "/coming-soon").permitAll())
+                .oauth2ResourceServer(oauth2ResourceServerCustomizer ->
+                        oauth2ResourceServerCustomizer.jwt(jwtCustomizer -> jwtCustomizer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+        return http.build();
+    }
+
+
+}
